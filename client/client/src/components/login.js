cd0c1931ff128232b8b21375f995dfa4c34cf7a6
Änjana Pathirana <anjanamadhushan44@gmail.com>
2023-01-24 12:29:15 +0530

    Login form handling init

--- client/client/src/components/login.js
+++ client/client/src/components/login.js
@@ -1,7 +1,43 @@
-import React from "react";
+import React, { Component } from "react";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
 
-const Login = () => {
-  return <h1>Login</h1>;
-};
+class Login extends Component {
+  state = {
+    account: { username: "", password: "" },
+  };
+  handleChange = (e) => {
+    const account = { ...this.state.account };
+    account.username = e.currentTarget.value;
+    this.setState({ account });
+  };
+  handleSubmit = (e) => {
+    e.preventDefault();
 
+    console.log("Submitted");
+  };
+  render() {
+    return (
+      <Form onSubmit={this.handleSubmit}>
+        <Form.Group className="mb-3" controlId="formBasicUsername">
+          <Form.Label>Username</Form.Label>
+          <Form.Control
+            autoFocus={true}
+            value={this.state.account.username}
+            onChange={this.handleChange}
+            type="text"
+            placeholder="Enter username"
+          />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control type="password" placeholder="Password" />
+        </Form.Group>
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
+      </Form>
+    );
+  }
+}
 export default Login;
