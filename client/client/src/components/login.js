d0157471882e8da3307b081b73933aa0841dca27
Änjana Pathirana <anjanamadhushan44@gmail.com>
2023-01-24 17:04:42 +0530

    Working on login page error

--- client/client/src/components/login.js
+++ client/client/src/components/login.js
@@ -1,49 +1,93 @@
 import React, { Component } from "react";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
+import Joi from "joi-browser";
 
 class Login extends Component {
   state = {
     account: { username: "", password: "" },
+    errors: {},
   };
-  handleChange = (e) => {
-    const account = { ...this.state.account };
-    account[e.currentTarget.name] = e.currentTarget.value;
-    this.setState({ account });
+
+  schema = {
+    username: Joi.string().required().label("Username"),
+    password: Joi.string().required().label("Password"),
   };
+
+  validate = () => {
+    const { error } = Joi.validate(this.state.account, this.schema, {
+      abortEarly: false,
+    });
+    if (!error) return null;
+
+    const errors = {};
+    for (let item of error.details) errors[item.path[0]] = item.message;
+    return errors;
+  };
+
+  validateProperty = ({ name, value }) => {
+    const obj = { [name]: value };
+    const schema = { [name]: this.schema[name] };
+    const { error } = Joi.validate(obj, schema);
+    return error ? error.details[0].message : null;
+  };
+
   handleSubmit = (e) => {
     e.preventDefault();
 
-    console.log("Submitted");
+    const errors = this.validate();
+    this.setState({ errors: errors || {} });
+    if (errors) return;
+
+    // console.log("Submitted");
   };
+
+  handleChange = ({ currentTarget: input }) => {
+    const errors = { ...this.state.errors };
+    const errorMessage = this.validateProperty(input);
+    if (errorMessage) errors[input.name] = errorMessage;
+    else delete errors[input.name];
+
+    const account = { ...this.state.account };
+    account[input.name] = input.value;
+
+    this.setState({ account, errors });
+  };
+
   render() {
     return (
-      <Form onSubmit={this.handleSubmit}>
-        <Form.Group className="mb-3" controlId="formBasicUsername">
-          <Form.Label>Username</Form.Label>
-          <Form.Control
-            autoFocus={true}
-            value={this.state.account.username}
-            onChange={this.handleChange}
-            name="username"
-            type="text"
-            placeholder="Enter username"
-          />
-        </Form.Group>
-        <Form.Group className="mb-3" controlId="formBasicPassword">
-          <Form.Label>Password</Form.Label>
-          <Form.Control
-            type="password"
-            placeholder="Password"
-            onChange={this.handleChange}
-            value={this.state.account.password}
-            name="password"
-          />
-        </Form.Group>
-        <Button variant="primary" type="submit">
-          Submit
-        </Button>
-      </Form>
+      <div>
+        <Form onSubmit={this.handleSubmit}>
+          <Form.Group className="mb-3" controlId="formBasicUsername">
+            <Form.Label>Username</Form.Label>
+            <Form.Control
+              name="username"
+              value={this.state.account.username}
+              autoFocus={true}
+              type="text"
+              label="Username"
+              onChange={this.handleChange}
+              placeholder="Enter username"
+              // error={errors.username}
+            />
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control
+              name="password"
+              value={this.state.account.password}
+              label="Password"
+              type="password"
+              placeholder="Password"
+              onChange={this.handleChange}
+              // error={errors.password}
+            />
+          </Form.Group>
+          <Button variant="primary" type="submit">
+            Submit
+          </Button>
+        </Form>
+      </div>
     );
   }
 }
