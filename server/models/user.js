b5f25f7b0cc50e542ffcc1b784cd733af578d546
Änjana Pathirana <anjanamadhushan44@gmail.com>
2023-01-25 15:22:10 +0530

    User model init

---
+++ server/models/user.js
@@ -0,0 +1,31 @@
+const Joi = require("joi");
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    minlength: 5,
+    maxlength: 255,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 8,
+    maxlength: 1024,
+  },
+});
+
+const User = mongoose.model("User", userSchema);
+
+const validateUser = (user) => {
+  const schema = {
+    username: Joi.string().min(5).max(255).required(),
+    password: Joi.string().min(8).max(1024).required(),
+  };
+  return Joi.validate(user, schema);
+};
+
+exports.User = User;
+exports.validate = validateUser;
