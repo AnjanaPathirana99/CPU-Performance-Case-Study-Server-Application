8fd2299089ee1e3c8e0fa7c3790b26e9edaf55f1
Änjana Pathirana <anjanamadhushan44@gmail.com>
2023-02-01 17:04:15 +0530

    Route cpuUsage init

--- client/client/src/App.js
+++ client/client/src/App.js
@@ -11,31 +11,10 @@ const App = () => {
     <div>
       <Routes>
         <Route path="/login" element={<Login />} />
-        <Route path="/cpuUsageTable" element={<CpuUsageTable />} />
-        {/* Day end commit-above line */}
+        <Route path="/cpuUsage" element={<CpuUsageTable />} />
       </Routes>
     </div>
   );
 };
 
 export default App;
-
-// const socket = io.connect("http://localhost:3001"); //POrt number of the server
-
-// const sendMessage = () => {
-//   socket.emit("send message", { message: "Hi" });
-// };
-// useEffect(() => {
-//   socket.on("receive message", (data) => {
-//     alert(data.message);
-//   });
-// });
-// return (
-//   <div className="App">
-//     <form>
-//       <label htmlFor="fname">Message</label>
-//       <input type="text" id="fname" name="fname"></input>
-//       <button onClick={sendMessage}>send</button>
-//     </form>
-//   </div>
-// );
